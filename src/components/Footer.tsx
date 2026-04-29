b7a40ed8a06cc85d4478b57c2c69edd65729fa19
Anthony Perret <anthony.p@thewalkingnerds.com>
2026-04-29 20:24:19 +0200

    feat: Footer improv

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Footer() {
 
   return (
     <footer>
       <div className="w-full flex items-center text-ivoire/66 font-dm-sans bg-acier px-40 py-10 border-t border-ivoire/66">
 
+        
         <div className="flex-1 text-ivoire font-bold text-2xl font-bebas-neue">
-          FAR<span className="text-cuivre">GA</span>
+          <Link href="/">
+            FAR<span className="text-cuivre">GA</span>
+          </Link>
         </div>
 
         <span className="text-xs">© 2026 FARGA — Tous droits réservés</span>
 
         <ul className="flex-1 flex justify-end gap-5 text-sm">
-          <li>Mentions légales</li>
-          <li>CGV</li>
-          <li>Contact</li>
+          <li><Link href="/mentions-legales">Mentions légales</Link></li>
+          <li><Link href="/politique-de-confidentialite">Politique de confidentialité</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
 
       </div>
