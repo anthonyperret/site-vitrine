1eda542f67ef68be7f7d10c3b747a500a5fce2be
Anthony Perret <anthony.p@thewalkingnerds.com>
2026-04-30 18:32:45 +0200

    feat: add vercel analytics to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, DM_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -39,6 +40,7 @@ export default function RootLayout({
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
+        <Analytics />
       </body>
     </html>
   );
