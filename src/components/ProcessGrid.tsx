bbc99df7d889aa078e09c5455866791653e2dea5
Anthony Perret <anthony.p@thewalkingnerds.com>
2026-05-05 16:58:14 +0200

    chore: wording in ProcessGrid component

--- src/components/ProcessGrid.tsx
+++ src/components/ProcessGrid.tsx
@@ -5,13 +5,13 @@ export default function ProcessGrid() {
   return(
     <div className="grid lg:grid-cols-4 gap-1 lg:mt-30 mt-10">
 
-      <ProcessItem number="01" title="Découverte" desc="On se rencontre pour comprendre votre métier, vos clients, vos objectifs. Pas de jargon, juste une conversation simple." i={0} />
+      <ProcessItem number="01" title="Découverte" desc="On se rencontre pour comprendre votre métier, vos clients et vos objectifs. Un échange simple pour poser les bonnes bases." i={0} />
 
-      <ProcessItem number="02" title="Conception" desc="Je conçois la maquette de votre site et vous la présente avant tout développement. Vous validez, on ajuste." i={1} />
+      <ProcessItem number="02" title="Conception" desc="Je conçois la maquette de votre site et vous la présente avant tout développement. On ajuste, vous validez." i={1} />
 
-      <ProcessItem number="03" title="Développement" desc="Je construis votre site avec soin, en respectant les délais convenus. Vous suivez l'avancement en temps réel." i={2} />
+      <ProcessItem number="03" title="Développement" desc="Je construis votre site avec soin, en respectant les délais convenus. Vous êtes tenu informé à chaque étape clé." i={2} />
 
-      <ProcessItem number="04" title="Livraison" desc="Mise en ligne, formation à l'outil, support post-livraison. Votre site vous appartient entièrement." i={3} />
+      <ProcessItem number="04" title="Livraison" desc="Mise en ligne, formation à l'outil si nécessaire et support post-livraison. Votre site vous appartient entièrement." i={3} />
 
     </div>
   )
