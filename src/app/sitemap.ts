f01253720c6d686c4c980b7f12d1f7142b41887d
Anthony Perret <anthony.p@thewalkingnerds.com>
2026-05-05 21:25:50 +0200

    chore: sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -32,5 +32,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.6,
     },
+    {
+      url: 'https://farga.me/mentions-legales',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
   ]
 }
